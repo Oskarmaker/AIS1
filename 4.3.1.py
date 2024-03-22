482b12d62a55c0c673367c34ce2e66fc61d31e4f
Student <93788062+Oskarmaker@users.noreply.github.com>
2024-03-22 11:48:27 +0300

    4.3.1.py

--- 4.3.1.py
+++ 4.3.1.py
@@ -1,16 +1,14 @@
 from random import random
 from matplotlib import pyplot as plt
 sp=[random() for i in range(10)]
-print(sp)
+plt.scatter(sp,[i for i in range(10)])
+plt.show()
 s=sum(sp)
 l=len(sp)
 sr=s/l
 sp.sort()
-print(sp)
 if l%2==0:
     sm=(sp[l//2]+sp[l//2+1])/2
 else:
     sm=sp[l//2]
-print(sr,sm)
-plt.scatter(sp,[i for i in range(10)])
-plt.show()
+print(f'{sr}-среднее значение,{sm}-медианное значение')
